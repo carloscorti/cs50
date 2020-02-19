81520ce4e953954de2992eba1a543ee5be7152a3
carloscorti <60133271+carloscorti@users.noreply.github.com>
2020-02-19 22:04:00 +0100

    project0 solution
    I add an item ('li') for each "new TODO", extra features: every new TODO has its own id number in text content, each new TODO has a delet button that delets this specific new TODO, when you delete any task, if you add new one by clicking "NEW" this last one added has a correlative id number regarding the last task remaining

---
+++ script.js
@@ -0,0 +1,49 @@
+const classNames = {
+    TODO_ITEM: 'todo-container',
+    TODO_CHECKBOX: 'todo-checkbox',
+    TODO_TEXT: 'todo-text',
+    TODO_DELETE: 'todo-delete',
+}
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+
+
+list.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'BUTTON') {
+        event.target.parentElement.remove()
+    }
+})
+
+function newTodo() {
+    //alert('New TODO button clicked!')
+
+    const listCount = list.querySelectorAll('li').length
+
+    const item = document.createElement('li')
+    if (listCount) {
+        const lastTask = list.lastChild.textContent
+        item.textContent = "New TODO Nº " + (parseInt(lastTask.substr(12)) + 1)
+    } else {
+        item.textContent = "New TODO Nº " + (listCount + 1)
+        //item.textContent = lastTask
+    }
+    const deleteItem = document.createElement('button')
+    deleteItem.textContent = "Delete Task"
+    item.appendChild(deleteItem)
+    list.appendChild(item)
+
+}
+
+
+
+/*
+function deleteItem(event) {
+    const clickSpot = event.target
+    if (clickSpot.nodeName === 'BUTTON'){
+        //clickSpot.parentElement.remove()
+    //}
+}
+*/
